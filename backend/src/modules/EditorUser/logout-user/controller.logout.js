58518b22e4170897d649fa2c35df2a6b80069df2
ostwald <lg.slauth@gmail.com>
2026-05-11 17:06:30 -0300

    Logout agora nos 3 cookies possiveis do sistema

--- backend/src/modules/EditorUser/logout-user/controller.logout.js
+++ backend/src/modules/EditorUser/logout-user/controller.logout.js
@@ -4,14 +4,33 @@ const isProd = process.env.NODE_ENV === 'prod';
 async function logout_controller_user(req,res,next) {
     try{
 
-        res.cookie('token', '', {
+        res.cookie('RefreshToken', '', {
             httpOnly: true,
             // Fica true apenas em produção (HTTPS)
             secure: isProd, 
             // 'lax' para o mesmo domínio, ou 'none' se precisar de cross-site no futuro
             sameSite: 'lax', 
-            maxAge: new Date(0) // expira imediatamente
+            expires: new Date(0) // expira imediatamente
         });
+
+        res.cookie("AcessToken", "", {
+            httpOnly: true,
+            // Fica true apenas em produção (HTTPS)
+            secure: isProd, 
+            // 'lax' para o mesmo domínio, ou 'none' se precisar de cross-site no futuro
+            sameSite: 'lax', 
+            expires: new Date(0) // expira imediatamente
+        })
+
+        res.cookie("VaultCookie", "", {
+            httpOnly: true,
+            // Fica true apenas em produção (HTTPS)
+            secure: isProd, 
+            // 'lax' para o mesmo domínio, ou 'none' se precisar de cross-site no futuro
+            sameSite: 'lax', 
+            expires: new Date(0) // expira imediatamente
+        })
+
 
         res.status(200).json({
             message: "Logout realizado com sucesso!."
